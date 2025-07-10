2148a51108e1c5b807399013ff28eef6b5e1df5f
futurecoder-gif <jeeendgame501@gmail.com>
2025-07-10 18:53:07 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22849789"))
-API_HASH = environ.get("API_HASH", "0fc127c6055acd59f00ec6c229e1e3c4")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7932500734:AAE8a2qrZfjTTslR3OWSy1z8w1JGHnVZDAk")
-OWNER = int(environ.get("OWNER", "7296271316"))
-CREDIT = "𝓨𝓪𝓼𝓼𝓾 𝓣𝓱𝓪𝓴𝓾𝓻"
-LOG_CHANNEL = "--1002477114210" # log group me bot ko admin narur banaye full rights ke sath
+API_ID = int(environ.get("API_ID", "28996064"))
+API_HASH = environ.get("API_HASH", "e09920a63e5f157b85a64ddba66596c6")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6880990182:AAGJ-FzHH1MsWQ_i5uUdMxvmCNG7-h0l2p0")
+OWNER = int(environ.get("OWNER", "6005627579"))
+CREDIT = "𝗧𝗲𝗰𝗵𝘆 𝗥𝗮𝘁𝗵𝗼𝗿𝗲"
+LOG_CHANNEL = "--1002783151454" # log group me bot ko admin narur banaye full rights ke sath
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
